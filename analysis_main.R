d07bdcfefc365498fb47c9042cf15a107a019f80
ricpie <ricpie@gmail.com>
2020-11-21 21:20:50 -0700

    updated plots

--- analysis_main.R
+++ analysis_main.R
@@ -14,11 +14,10 @@ registerDoParallel(cores=detectCores()-1)
 
 
 # Define rough regions. 
-fence = data.frame(region = c("denver","nyc"),
-                  denver_lat_range = c(39,41),
-                  denver_lon_range = c(-104,-106),
-                  nyc_lat_range = c(40,42),
-                  nyc_lon_range = c(-74,-76))
+denver_lat_range = c(39,41)
+denver_lon_range = c(-106,-104)
+nyc_lat_range = c(40,42)
+nyc_lon_range = c(-76,-73)
 
 #Import PA data. Use with api later?
 all_pa_paths <- list.files(
@@ -43,7 +42,8 @@ import_pa <-function(x){
                   trimmed_filename = sub("\\)", "", trimmed_filename),
                   trimmed_filename = sub("\\(", "", trimmed_filename),
                   trimmed_filename = sub("\\)", "", trimmed_filename)) %>% 
-    separate(trimmed_filename,sep = " ",into = c("enviro","lat","lon","sensor","aveperiod","c","d"))
+    separate(trimmed_filename,sep = " ",into = c("enviro","lat","lon","sensor","aveperiod","c","d")) %>% 
+    dplyr::mutate(enviro = tolower(enviro))
 }
 
 pa_data <- ldply(all_pa_paths,import_pa,.parallel = TRUE) %>%
@@ -51,30 +51,44 @@ pa_data <- ldply(all_pa_paths,import_pa,.parallel = TRUE) %>%
   dplyr::mutate(datetime = as.POSIXct(created_at,tz = "UTC"),
                 lat = as.numeric(lat),
                 lon = as.numeric(lon),
-                year = year(datetime)) %>% 
+                year = as.factor(year(datetime))) %>% 
+  dplyr::filter(`PM1.0_CF1_ug/m3` <4000,
+                `PM2.5_CF1_ug/m3` <4000,
+                `PM10.0_CF1_ug/m3`<4000) %>% 
   dplyr::select(-c("created_at","IAQ","ADC","c","d","UptimeMinutes")) %>% 
-  dplyr::group_by(location_name,datetime) %>% 
+  dplyr::group_by(location_name,datetime,enviro) %>% 
   dplyr::mutate(`PM1.0_CF1_ug/m3` = mean(`PM1.0_CF1_ug/m3`,na.rm = T),
                 `PM2.5_CF1_ug/m3` = mean(`PM2.5_CF1_ug/m3`,na.rm = T),
                 `PM10.0_CF1_ug/m3` = mean(`PM10.0_CF1_ug/m3`,na.rm = T)) %>% 
   dplyr::filter(row_number()==1) %>% 
-  dplyr::mutate(region = case_when(lat %between% fence$denver_lat_range &
-                                     lon %between% fence$denver_lon_range ~ "denver",
-                                   lat %between% fence$nyc_lat_range &
-                                     lon %between% fence$nyc_lon_range ~ "nyc",
+  dplyr::mutate(region = case_when(lat %between% denver_lat_range &
+                                     lon %between% denver_lon_range ~ "denver",
+                                   lat %between% nyc_lat_range &
+                                     lon %between% nyc_lon_range ~ "nyc",
                                    TRUE ~ "Other"))
 
 
-#Use the average of these channels as per Mailings et al. 2019b.  Also filter out data above 4000 when doing the average
-# pa_data[, pm2_5_cf_1_ave := dt_case_when(pm2_5_cf_1 <4000 & pm2_5_cf_1_b<4000 ~ rowMeans(pa_data[,c('pm2_5_cf_1','pm2_5_cf_1_b')],na.rm = TRUE) , #PM25 kitchen uses mostly ECM, but some corrected PATS data.
-#                                          pm2_5_cf_1 <4000  ~ pm2_5_cf_1,
-#                                          pm2_5_cf_1_b <4000  ~ pm2_5_cf_1_b)]
+plot_timeseries <- pa_data %>% 
+  dplyr::filter(!region %in% "Other") %>% 
+  dplyr::group_by(datetime,region,enviro,year) %>% 
+  dplyr::summarise(meanpm25 = mean(`PM2.5_CF1_ug/m3`,na.rm = T)) %>% 
+  ggplot(aes(x=datetime,y=meanpm25,color = year)) + 
+  geom_line(alpha = 0.1) +
+  geom_smooth(alpha = 0.1) +
+  facet_grid(region~enviro) +
+  ylim(c(0,100)) +
+  ggtitle("Regional averaged PM2.5")
+plot_timeseries
 
-plot_scatter_all <- pa_data %>% 
-  ggplot(aes(x=datetime,y=`PM2.5_CF1_ug/m3`,color = )) + 
-  geom_point(alpha = 0.1) +
-  geom_smooth(alpha = 0.2) +
-  facet_grid(year~enviro) +
-  ggtitle(paste0(path_region," PM2.5"))
-plot_scatter_all
+plot_box <- pa_data %>% 
+  dplyr::filter(!region %in% "Other") %>% 
+  dplyr::group_by(datetime,region,enviro,year) %>% 
+  dplyr::summarise(meanpm25 = mean(`PM2.5_CF1_ug/m3`,na.rm = T)) %>% 
+  ggplot(aes(x=datetime,y=`PM2.5_CF1_ug/m3`,color = year)) + 
+  geom_jitter(alpha = 0.1,width = .3) +
+  geom_boxplot(alpha = 0.2) +
+  facet_grid(region~enviro) 
+plot_box
+
+
 
